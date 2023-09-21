faa27e63d127350723452000ba4c21043a11f663
JingmingYangJimmy <123596780+JingmingYangJimmy@users.noreply.github.com>
2023-09-20 22:25:19 -0500

    Jimmy changes

--- exercise #2/JAVA/NumericErrors/src/NumericErrors.java
+++ exercise #2/JAVA/NumericErrors/src/NumericErrors.java
@@ -1,5 +1,16 @@
+//Wenpei Shao  and Jingming Yang
+//wshao33          jyang668
+
+/*
+ * This class test the overflow exception in four arithmetic notation
+ * 
+ */
 public class NumericErrors {
 
+    /*
+     * Takes two integers x and y, returns their sum(x+y). Throws an
+     * ArithmeticException in case of overflow.
+     */
     public static int add(int x, int y) throws ArithmeticException {
         // Check for positive overflow
         if (x > 0 && y > 0 && x > Integer.MAX_VALUE - y) {
@@ -12,12 +23,22 @@ public class NumericErrors {
         return x + y;
     }
 
+    /*
+     * Take two integers x and y, return their difference(x-y). Throws an
+     * ArithmeticException in case of overflow.
+     */
     public static int subtract(int x, int y) throws ArithmeticException {
         // Use add function to check for overflow
         return add(x, -y);
     }
 
-    public static int multiply(int x, int y) throws ArithmeticException {
+    /*
+     * Take two integers x and y, return their multiplication(x*y). Throws an
+     * ArithmeticException in case of overflow.
+     */
+    public static int multiply(int x, int y) throws ArithmeticException {// it is harder than sum or difference since
+                                                                         // the result
+        // will be more difficult to calculate and also need to consider sign
         if (y > 0 && x > Integer.MAX_VALUE / y) {
             throw new ArithmeticException("Positive overflow detected in multiplication");
         }
@@ -27,7 +48,14 @@ public class NumericErrors {
         return x * y;
     }
 
-    public static int divide(int x, int y) throws ArithmeticException {
+    /*
+     * Take two integers x and y, return their quotient (x/y). Throws an
+     * ArithmeticException in case of overflow.
+     */
+    public static int divide(int x, int y) throws ArithmeticException { // overflow detection is necessary
+        // Integer.MIN_VALUE is -2147483648 and Integer.MAX_VALUE is 2147483647 so when
+        // min_value divide by -1
+        // it will cause an overflow
         if (y == 0) {
             throw new ArithmeticException("Division by zero");
         }
@@ -37,6 +65,9 @@ public class NumericErrors {
         return x / y;
     }
 
+    /*
+     * Having tests here. This is the main method
+     */
     public static void main(String[] args) {
         // Test cases can be added here
     }
